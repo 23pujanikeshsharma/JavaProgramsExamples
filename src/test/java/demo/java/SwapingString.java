db45b8a488e9c9fbf0bef4857bb3c7ebf3bb834b
24pujakumarii@gmail.com <24pujakumarii@gmail.com>
2024-05-11 17:28:11 +0530

    Swaping of two string program completed

--- src/test/java/demo/java/SwapingString.java
+++ src/test/java/demo/java/SwapingString.java
@@ -4,6 +4,41 @@ public class SwapingString {
 
 	public static void main(String[] args) {
 		System.out.println("Swaping of two string");
+		
+		String s = "Hello";
+		
+		String t = " World";
+		
+     System.out.println(" Before swaping of two string value : ");
+		
+		System.out.println(" s : "+s);
+		System.out.println(" t : "+t);
+		
+		s = s+t;   // HelloWorld
+		
+		t = s.replace(t, "").trim();  // Hello
+		
+		s = s.replace(t, "");
+		System.out.println("######################################");
+
+		
+		System.out.println(" After swaping of two string value : ");
+		
+		System.out.println(" s : "+s);
+		System.out.print(" t : "+t);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+// 97 to 122 : A to Z
+// 65 to 90 : a to z
+// 48 to 57 : 0 to 9 		
+		
 
 	}
 
